4e0a05b2996a1ecf0ebfa67e4031adf9f6d133c6
coltonnewton89 <coltonnewton89@gmail.com>
2020-03-03 18:15:58 -0600

    finished i think?

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,26 @@
-const url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
+// const url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
+
+export const fetchMakes = () => {
+    return (dispatch) => {
+        fetch("https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json")
+            .then(res => res.json())
+            .then(response => {
+                const action = {
+                    type: 'FETCH_MAKES',
+                    value: response.Results
+                }
+                dispatch(action)
+            })
+    }
+}
+
+export const deleteMake = (index) =>{
+    
+    return{
+        type: 'DELETE_MAKE',
+        value: index
+    }
+}
 
 export const addCar = (car) => {
     return {
@@ -13,3 +35,4 @@ export const removeCar = (index) => {
         value: index
     }
 }
+
